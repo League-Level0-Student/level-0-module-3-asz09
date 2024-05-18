46d2cd94c8ac08e97450be56f7721463e64b9a5c
league <clarkkent4027@gmail.com>
2024-05-17 18:25:04 -0700

    odfuvdigiortiorjbpn[yrhb

---
+++ src/_01_else_if/_4_are_you_happy/AreYouHappy.java
@@ -0,0 +1,27 @@
+package _01_else_if._4_are_you_happy;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
+public class AreYouHappy {
+
+	public static void main(String[] args) {
+	String feelings = JOptionPane.showInputDialog(null,"are you happy");
+	if(feelings == "yes") {
+		JOptionPane.showMessageDialog(null,"Keep doing whatever you're doing.");
+	}
+	
+	
+	else {
+		String morefeelings = JOptionPane.showInputDialog(null,"Do you want to be happy?");
+		if(morefeelings == "yes") {
+			JOptionPane.showMessageDialog(null, "Change Something");		
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "Keep doing whatever you're doing");
+		}
+	}
+		
+	}
+
+}
