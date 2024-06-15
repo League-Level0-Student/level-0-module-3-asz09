d71d3308692fc1dfb82942362554e29cc33be302
league <yt>
2024-06-14 18:24:05 -0700

    progress.

--- src/_02_loop_variables/_1_years_alive/YearsAlive.java
+++ src/_02_loop_variables/_1_years_alive/YearsAlive.java
@@ -3,5 +3,11 @@ package _02_loop_variables._1_years_alive;
 public class YearsAlive {
 public static void main(String[] args) {
 	
+	int year=2009;
+	int yearsAlive=2024-year;
+	for (int i=0; i <= yearsAlive; i++) {
+		System.out.println(i + year);   
+		
+	}
 }
 }
